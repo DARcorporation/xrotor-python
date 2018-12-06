f1f2c88d5fd496fded2326cf04a1070aa7013876
Daniel de Vries <daniel.devries@darcorp.com>
2018-12-06 15:36:30 -0600

    Added OFast compiling option to setup.py.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import glob
 from numpy.distutils.core import Extension
 
-ext = Extension(name='xrotor', sources=glob.glob("src/*.f90"))
+ext = Extension(name='xrotor', sources=glob.glob("src/fortran/*.f90"), extra_f90_compile_args=['-Ofast'])
 
 if __name__ == '__main__':
     from numpy.distutils.core import setup
