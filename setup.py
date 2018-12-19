06f71e75bc5099f58c16822814f8e9ad39d8180c
Daniel de Vries <daniel.devries@darcorp.com>
2018-12-19 14:05:35 -0600

    Deleted old stuff.

--- setup.py
+++
@@ -1,9 +0,0 @@
-import glob
-from numpy.distutils.core import Extension, setup
-
-flib = Extension(name='xrotor.flib', sources=glob.glob("xrotor/src_fortran/*.f90"), extra_f90_compile_args=['-Ofast'])
-
-if __name__ == '__main__':
-    setup(name='xrotor',
-          packages=['xrotor'],
-          ext_modules=[flib])
--- xrotor/__init__.py
+++
@@ -1,1 +0,0 @@
-from xrotor.interface import *
